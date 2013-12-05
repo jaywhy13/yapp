b9861b8af69236da766f6609ed1cc43dd2239df9
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-12-05 17:41:55 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='yapp',
+      version='0.1',
+      description='Yet Another Python Parsing - a simple parser built with Pyparsing',
+      url='http://github.com/jaywhy13/yapp',
+      author='Jean-Mark Wright',
+      author_email='jeanmark.wright@gmail.com',
+      license='MIT',
+      packages=['yapp'],
+      install_requires = [
+	      'Django==1.5.5',
+            'argparse==1.2.1',
+            'pyparsing==2.0.1',
+            'wsgiref==0.1.2',
+      ],
+      zip_safe=False)
